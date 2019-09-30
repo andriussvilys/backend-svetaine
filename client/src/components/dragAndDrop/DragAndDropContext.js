5edb0347b0f5a97ddc9ef3ace2ff1dbe0b4dc0d8
Your name hereAndrius <iciukas@gmail.com>
2019-09-30 18:13:10 +0200

    cascade improvements

--- client/src/components/dragAndDrop/DragAndDropContext.js
+++ client/src/components/dragAndDrop/DragAndDropContext.js
@@ -33,25 +33,13 @@ export default class NavigationInfo extends Component{
 
         if (startColumn === finishColumn){
             const column = this.state.columns[source.droppableId];
-            console.log('COLUMN')
-            console.log(column.id)
-            console.log(this.state.columnOrder)
-            console.log(this.state.columnOrder.indexOf(column.id))
-            let newFileIds = Array.from(column.fileIds)
-            console.log("source")
-            console.log(source)
-            console.log("destination")
-            console.log(destination)
 
-            console.log("source INDEX")
-            console.log(source.index)
+            let newFileIds = Array.from(column.fileIds)
+
+
             newFileIds.splice(source.index - this.state.columnOrder.indexOf(column.id) * 4, 1)
 
-            console.log(newFileIds)
-            console.log('calculated Index')
-            console.log(destination.index)
-            newFileIds.splice(destination.index, 0, draggableId)
-            console.log(newFileIds)
+
     
             const newColumn = {
                 ...column,
@@ -71,83 +59,156 @@ export default class NavigationInfo extends Component{
         }
 
 
-        let startFileIds = Array.from(startColumn.fileIds);
-        let finishFileIds = Array.from(finishColumn.fileIds);
+        // let startFileIds = Array.from(startColumn.fileIds);
+        // let finishFileIds = Array.from(finishColumn.fileIds);
 
-        // const removeFromFinish = () => {
-        //     //if draggable moved to last position in the destination column
-        //     if(destination.index > finishColumn.fileIds.length-1){
-        //         //pop the first elemenet and return it at the end of source column
-        //         finishFileIds.shift()
-        //         startFileIds.push(finishColumn.fileIds[0])
-        //         return
-        //     }
-        //     else{
-        //         finishFileIds.pop()
-        //         startFileIds.unshift(finishColumn.fileIds[finishColumn.fileIds.length - 1])
+        // const rewriteFileIdsArray = () => {
+        //     startFileIds.splice(source.index - this.state.columnOrder.indexOf(startColumn.id) * 4, 1);
+        //     finishFileIds.splice(destination.index, 0, draggableId);
+
+        //     console.log("source")
+        //     console.log(source)
+        //     console.log("destination")
+        //     console.log(destination)
+
+        // }
+
+        // rewriteFileIdsArray()
+
+        // const newStart = {
+        //     ...startColumn,
+        //     fileIds: startFileIds
+        // };
+
+        // // finishFileIds.splice(destination.index, 0, draggableId);
+
+        // const newFinish = {
+        //     ...finishColumn,
+        //     fileIds: finishFileIds,
+        // };
+
+        // const newState = {
+        //     ...this.state,
+        //     columns: {
+        //         ...this.state.columns,
+        //         [newStart.id]: newStart,
+        //         [newFinish.id]: newFinish,
         //     }
         // }
 
-        // removeFromFinish()
+            
+            let newState ={...this.state, columns: {...this.state.columns}}
 
-        const rewriteFileIdsArray = () => {
-            startFileIds.splice(source.index - this.state.columnOrder.indexOf(startColumn.id) * 4, 1);
-            finishFileIds.splice(destination.index, 0, draggableId);
+            const reorderFiles = () => {
 
-
-            console.log("source")
-            console.log(source)
-            console.log("destination")
-            console.log(destination)
-
-
-            // //if destination index is before last
-            // if(destination.index < finishFileIds.length -1){
-            //     finishFileIds.splice(destination.index, 0, draggableId);
-            //     startFileIds.splice(source.index - this.state.columnOrder.indexOf(startColumn.id) * 4, 1);
-            //     startFileIds.unshift(finishFileIds[finishFileIds.length-1])
-            //     finishFileIds.pop()
+            // if(finishColumn.fileIds.length === 4){
             //     return
             // }
-            // //if destination is the last index
-            // else{
-            //     finishFileIds.shift()
-            //     startFileIds.push(finishFileIds[0])
-            //     finishFileIds.push(draggableId)
-            //     startFileIds.splice(source.index - this.state.columnOrder.indexOf(startColumn.id) * 4, 1);
-            //     console.log(this.state)
-            // }
-        }
 
-        rewriteFileIdsArray()
+            // const cascadeStartIndex = this.state.columnOrder.indexOf(finishColumn.id)
+            const columnOrder = this.state.columnOrder
 
-        const newStart = {
-            ...startColumn,
-            fileIds: startFileIds
-        };
+            let cascadeStartIndex = this.state.columnOrder.indexOf(finishColumn.id)
+            let cascadeEndIndex = cascadeStartIndex + 1
 
-        // finishFileIds.splice(destination.index, 0, draggableId);
+            // console.log(this.state.columns[finishColumnId].fileIds)
 
-        const newFinish = {
-            ...finishColumn,
-            fileIds: finishFileIds,
-        };
+            let dummyColumns = this.state.columns
+            console.log('DUMMY COLUMNS')
+            console.log(dummyColumns)
 
-        const newState = {
-            ...this.state,
-            columns: {
-                ...this.state.columns,
-                [newStart.id]: newStart,
-                [newFinish.id]: newFinish,
+
+            //check if element goes up or down
+            //if destination is below 
+            if(this.state.columnOrder.indexOf(startColumn.id) < this.state.columnOrder.indexOf(finishColumn.id)){
+
+                for(var i = 0; i < columnOrder.length; i++  ){
+                    console.log(`start counter is ${cascadeStartIndex}`)
+                    console.log(`finish counter is ${cascadeEndIndex}`)
+                    console.log(`columnorder[currentCounter]`)
+                    console.log(columnOrder[cascadeStartIndex])
+
+                    //first duplicate the last element on source array
+                    if(cascadeEndIndex > columnOrder.length - 1 ){
+                        cascadeEndIndex = 0
+                    }
+                    if(cascadeStartIndex > columnOrder.length - 1 ){
+                        cascadeStartIndex = 0
+                    }
+
+
+                    let cascadeStartArray = dummyColumns[columnOrder[cascadeStartIndex]].fileIds
+                    let cascadeFinishArray = dummyColumns[columnOrder[cascadeEndIndex]].fileIds
+                    
+                    //put the last element of finish column to first position in start column
+                    console.log(`FINISH ARRAY B4 MUTATION ${cascadeFinishArray}`)
+                    cascadeFinishArray.unshift(cascadeStartArray[cascadeStartArray.length -1])
+                    console.log(`FINISH ARRAY after MUTATION ${cascadeFinishArray}`)
+                    const startStateTarget = columnOrder[cascadeStartIndex]
+                    console.log(`startStateTarget ${columnOrder[cascadeStartIndex]}`)
+                    cascadeStartIndex += 1;
+
+                    //THEN POP THE LAST ELEMENT FROM THE SOURCE ARRAY
+
+                    console.log(`Start ARRAY before mutation ${cascadeStartArray}`)
+                    cascadeStartArray.pop()
+                    console.log(`Start ARRAY after mutation ${cascadeStartArray}`)
+                    const finishStateTarget = columnOrder[cascadeEndIndex]
+                    cascadeEndIndex += 1;
+
+                    newState.columns[startStateTarget].fileIds = cascadeStartArray
+                    newState.columns[finishStateTarget].fileIds = cascadeFinishArray
+                    console.log(`END OF CYCLE ${i + 1}`)
+                    }
+                    
+                }
+            else{
+                // let cascadeStartArray = dummyColumns[columnOrder[cascadeStartIndex]].fileIds
+                // let cascadeFinishArray = dummyColumns[columnOrder[cascadeEndIndex]].fileIds
+
+                // const startStateTarget = columnOrder[cascadeStartIndex]
+                // const finishStateTarget = columnOrder[cascadeEndIndex]
+
+                // console.log(`cascadeStart ${cascadeStartIndex}`)
+                // console.log(`cascadeFinish ${cascadeEndIndex}`)
+
+                // cascadeFinishArray.unshift(cascadeFinishArray[cascadeFinishArray.length -1])
+                // // cascadeStartArray.unshift(cascadeStartArray[cascadeStartArray.length -1])
+
+                // //THEN POP THE LAST ELEMENT FROM THE SOURCE ARRAY
+                // cascadeFinishArray.pop()
+                // cascadeFinishArray.splice(source.index, 1)
+
+                // newState.columns[startStateTarget].fileIds = cascadeStartArray
+                // newState.columns[finishStateTarget].fileIds = cascadeFinishArray
+
+                // startColumn = this.state.columns[source.droppableId]
+                // finishColumn = this.state.columns[destination.droppableId]
+
+                let sourceArray = this.state.columns[source.droppableId].fileIds
+                let finishArray = this.state.columns[destination.droppableId].fileIds
+
+                sourceArray.unshift(finishArray[finishArray.length - 1])
+                finishArray.pop()
+
+                // let newStart = startColumn.fileIds
+                // newStart.unshift(finishColumn.fileIds[finishColumn.fileIds.length - 1])
+                // let newFinish = finishColumn.fileIds
+                // newFinish.pop()
+                newState.columns[source.droppableId].fileIds = sourceArray
+                newState.columns[destination.droppableId].fileIds = finishArray
             }
+                // this.setState({newState})
+                console.log("new STATE")
+                console.log(newState)
+                this.setState(newState)
+            }
+
+
+            this.setState(newState, reorderFiles())
+            return
         }
 
-        this.setState(newState)
-        return
-
-
-
-    }
 
     render(){
         return(
