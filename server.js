24bc5d1d5597fdeee63d3fbbd04860deddf72a6d
Your name hereAndrius <iciukas@gmail.com>
2019-12-11 14:46:39 +0100

    build test

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path')
 
 const artworkInfo = require('./routes/api/artworkInfo');
 const familySetup = require('./routes/api/familySetup');
@@ -44,6 +45,13 @@ app.use('/fetchImages', fetchImages);
 app.use('/deleteImage', deleteImage);
 // this uses a folder inside the server
 // app.use(express.static('files'))
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+    app.get('*', ((req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    }))
+}
 
 const port = process.env.PORT || 5000;
 
