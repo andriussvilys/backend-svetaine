d6f1e9e820a3dab5c2638fc049e20a5d4764a9ee
Your name hereAndrius <iciukas@gmail.com>
2020-01-29 16:18:49 +0100

    imagemin route

--- routes/imagemin.js
+++ routes/imagemin.js
@@ -7,20 +7,20 @@ const imageminJpegtran = require('imagemin-jpegtran');
 const imageminPngquant = require('imagemin-pngquant');
 const imageminMozjpeg = require('imagemin-mozjpeg');
  
-router.post(`/`, (req, res, next) => {
+router.post(`/:directory`, (req, res, next) => {
     (async () => {
-        const files = await imagemin([`./client/public/uploads/*.jpg`], {
-            destination: './client/public/uploads/imagemin',
+        const files = await imagemin([`./client/public/uploads/${req.params.directory}/*.{jpg,png}`], {
+            destination: `./client/public/uploads/${req.params.directory}`,
             plugins: [
-                imageminMozjpeg({progressive: true, quality: 0.8}),
+                imageminMozjpeg({progressive: true, quality: 80}),
                 imageminPngquant({
                     quality: [0.6, 0.8],
                     strip: true
                 })
             ]
         });
-        
-        console.log(files);
+
+        return res.send(`operation complete in ${req.params.directory}`)
         
         //=> [{data: <Buffer 89 50 4e …>, destinationPath: 'build/images/foo.jpg'}, …]
     })();
