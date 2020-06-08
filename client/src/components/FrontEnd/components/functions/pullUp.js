64fddd301ec91f1ba4df3a82c55f9135996304c7
Your name hereAndrius <iciukas@gmail.com>
2020-06-08 13:50:00 +0200

    pullUp v2

--- client/src/components/FrontEnd/components/functions/pullUp.js
+++ client/src/components/FrontEnd/components/functions/pullUp.js
@@ -1,109 +1,187 @@
 const pullUp = (options) => {
 
-    const onTouch = e => {
-        console.log(e.target)
-    }
-    const parent = document.getElementById(options.parentId)
-    const child = document.getElementById(options.childId)
+//     const onTouch = e => {
+//         console.log(e.target)
+//     }
+//     const parent = document.getElementById(options.parentId)
+//     const child = document.getElementById(options.childId)
 
-    if(!child || !parent){
-        console.log("parent or child not present")
-        return
-    }
+//     if(!child || !parent){
+//         console.log("parent or child not present")
+//         return
+//     }
 
-    const childPositionY = () => {
-        return parent.getBoundingClientRect().height - child.getBoundingClientRect().y + 4
-    } 
-
-
-    let startPositionY = null
-    let endPositionY = null
-    let drag = null
-    let transformValue = 0
+//     const childPositionY = () => {
+//         return parent.getBoundingClientRect().height - child.getBoundingClientRect().y + 4
+//     } 
 
 
-    const moveEvent = (e) => {
-        if(drag){
-            let diff = null
-            if(!e.touches){
-            diff = startPositionY - e.clientY
-            }
-            else{
-            diff = startPositionY - e.touches[0].clientY
-            }
-            transformValue = -diff
+//     let startPositionY = null
+//     let endPositionY = null
+//     let drag = null
+//     let transformValue = 0
 
-            if(endPositionY){
-                transformValue = -endPositionY - diff
-                // console.log("endPositionY")
-                // console.log(endPositionY)
-            }
-            if(-transformValue >= child.clientHeight){
-                // console.log("transformValue >= child.clientHeight")
-                return
-            }
-            if(transformValue > 0){
-                // console.log("transformValue > 0")
-                return
-            }
-            if(endPositionY){
-                if(-transformValue < 10){
-                    transformValue = 0
-                }
-            }
-            if(-transformValue >= child.clientHeight - 20){
-                transformValue = -child.clientHeight
-            }
-            // console.log("transformValue")
-            // console.log(transformValue)
+
+//     const moveEvent = (e) => {
+//         if(drag){
+//             let diff = null
+//             if(!e.touches){
+//             diff = startPositionY - e.clientY
+//             }
+//             else{
+//             diff = startPositionY - e.touches[0].clientY
+//             }
+//             transformValue = -diff
+
+//             if(endPositionY){
+//                 transformValue = -endPositionY - diff
+//                 // console.log("endPositionY")
+//                 // console.log(endPositionY)
+//             }
+//             if(-transformValue >= child.clientHeight){
+//                 // console.log("transformValue >= child.clientHeight")
+//                 return
+//             }
+//             if(transformValue > 0){
+//                 // console.log("transformValue > 0")
+//                 return
+//             }
+//             if(endPositionY){
+//                 if(-transformValue < 10){
+//                     transformValue = 0
+//                 }
+//             }
+//             if(-transformValue >= child.clientHeight - 20){
+//                 transformValue = -child.clientHeight
+//             }
+//             // console.log("transformValue")
+//             // console.log(transformValue)
             
-            transformValue = Math.round(transformValue)
-                            // console.log("transformValue")
-                            // console.log(transformValue)
+//             transformValue = Math.round(transformValue)
+//                             // console.log("transformValue")
+//                             // console.log(transformValue)
             
 
-            child.style.transform = `translateY(${transformValue}px)`
-        }
+//             child.style.transform = `translateY(${transformValue}px)`
+//         }
+//     }
+
+//     const startEvent = (e) => {
+//         drag = true       
+//             if(e.touches){
+//                 startPositionY = e.touches[0].clientY
+//                 parent.addEventListener("touchmove", e => {
+//                     moveEvent(e)
+//                 })
+//             }
+//             else{
+//                 startPositionY = e.clientY
+//                 parent.addEventListener("mousemove", e => {
+//                     moveEvent(e)
+//                 })
+//             }
+//     }
+
+//     const endEvent = (e) => {
+
+//         endPositionY = Math.round(childPositionY() - 50)
+//         // if(endPositionY >= 0){
+//         //     endPositionY = null
+//         // }
+//         if(endPositionY <= 0){
+//             child.classList.remove("info-up")
+//         }
+//         else{
+//             child.classList.add("info-up")
+//         }
+//         // console.log(`endposition ${endPositionY}`)
+//         drag = false
+//     }
+
+//     parent.addEventListener("mousedown", (e) => startEvent(e))
+//     parent.addEventListener("mouseup", (e) => endEvent(e))
+
+//     parent.addEventListener("touchstart", (e) => startEvent(e))
+//     parent.addEventListener("touchend", (e) => endEvent(e))
+
+// }
+
+
+    // var dragItem = document.querySelector("#item");
+    // var container = document.querySelector("#container");
+
+    const container = document.getElementById(options.parentId)
+    const dragItem = document.getElementById(options.childId)
+
+    var active = false;
+    var currentX;
+    var currentY = null;
+    var initialX;
+    var initialY;
+    var xOffset = 0;
+    var yOffset = 0;
+
+    container.addEventListener("touchstart", dragStart, false);
+    container.addEventListener("touchend", dragEnd, false);
+    container.addEventListener("touchmove", drag, false);
+
+    container.addEventListener("mousedown", dragStart, false);
+    container.addEventListener("mouseup", dragEnd, false);
+    container.addEventListener("mousemove", drag, false);
+
+    function dragStart(e) {
+        console.log("drag start")
+    if (e.type === "touchstart") {
+        initialX = e.touches[0].clientX - xOffset;
+        initialY = e.touches[0].clientY - yOffset;
+    } else {
+        initialX = e.clientX - xOffset;
+        initialY = e.clientY - yOffset;
+    }
+    active = true
     }
 
-    const startEvent = (e) => {
-        drag = true       
-            if(e.touches){
-                startPositionY = e.touches[0].clientY
-                parent.addEventListener("touchmove", e => {
-                    moveEvent(e)
-                })
-            }
-            else{
-                startPositionY = e.clientY
-                parent.addEventListener("mousemove", e => {
-                    moveEvent(e)
-                })
-            }
+    function dragEnd(e) {
+        console.log("drag end")
+    initialX = currentX;
+    initialY = currentY;
+
+    active = false;
     }
 
-    const endEvent = (e) => {
+    function drag(e) {
+    if (active) {
+    
+        e.preventDefault();
+    
+        if (e.type === "touchmove") {
+        currentX = e.touches[0].clientX - initialX;
+        currentY = e.touches[0].clientY - initialY;
+        } else {
+        currentX = e.clientX - initialX;
+        currentY = e.clientY - initialY;
+        }
 
-        endPositionY = Math.round(childPositionY() - 50)
-        // if(endPositionY >= 0){
-        //     endPositionY = null
-        // }
-        if(endPositionY <= 0){
-            child.classList.remove("info-up")
+        xOffset = currentX;
+        yOffset = currentY;
+
+        if(-currentY > dragItem.clientHeight){
+            return
         }
-        else{
-            child.classList.add("info-up")
+        if(currentY > 0 ){
+            return
         }
-        // console.log(`endposition ${endPositionY}`)
-        drag = false
+        if(currentY > -20){
+            currentY = 0
+        }
+
+        setTranslate(currentX, currentY, dragItem);
+    }
     }
 
-    parent.addEventListener("mousedown", (e) => startEvent(e))
-    parent.addEventListener("mouseup", (e) => endEvent(e))
-
-    parent.addEventListener("touchstart", (e) => startEvent(e))
-    parent.addEventListener("touchend", (e) => endEvent(e))
-
+    function setTranslate(xPos, yPos, el) {
+    el.style.transform = `translateY(${yPos}px)`;
+    }
 }
 
 export default pullUp
