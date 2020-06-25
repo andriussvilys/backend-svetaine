abf28df90c74e768f766d2548655cc38e5e0a3ef
Your name hereAndrius <iciukas@gmail.com>
2020-06-25 18:01:02 +0200

    serve static with npm start

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
-  
+
 app.listen(port, () => {console.log(`server started on port ${port}`)});
 
 //DB config
@@ -69,9 +69,9 @@ app.use((err, req, res, next) => {
     res.status(500).send({error: err})
 })
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'))
-    app.get('*', ((req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    }))
-}
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static('client/build'))
+//     app.get('*', ((req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     }))
+// }
