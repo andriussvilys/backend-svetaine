51c5c63c1ba641d816b23d8a248ae3787bda8473
Your name hereAndrius <iciukas@gmail.com>
2020-06-25 16:17:58 +0200

    server.js

--- server.js
+++ server.js
@@ -62,15 +62,10 @@ app.use((err, req, res, next) => {
     res.status(500).send({error: err})
 })
 
-app.use(express.static('client/build'));
+// app.use(express.static('client/build'));
 
-app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+// app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
 
-// app.use(express.static(path.join(__dirname, 'build')));
-
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'))
