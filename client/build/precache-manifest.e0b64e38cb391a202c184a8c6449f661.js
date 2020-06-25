54e1b28a779d09fa0129c6be1a17b3f2bec51bf6
Your name hereAndrius <iciukas@gmail.com>
2020-06-25 16:22:49 +0200

    build

---
+++ client/build/precache-manifest.e0b64e38cb391a202c184a8c6449f661.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287ad3721ae6d6e3101f36404503332f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b47d4f437be2080430f1",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6447b3f74e2ca451b1c0",
+    "url": "./static/css/main.425b5fd5.chunk.css"
+  },
+  {
+    "revision": "b47d4f437be2080430f1",
+    "url": "./static/js/2.fc3eeed0.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "./static/js/2.fc3eeed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6447b3f74e2ca451b1c0",
+    "url": "./static/js/main.ef363857.chunk.js"
+  },
+  {
+    "revision": "11808710d3b9cbd7e7eb",
+    "url": "./static/js/runtime-main.2cfe3499.js"
+  }
+]);
