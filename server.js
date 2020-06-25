238da2c1d3e234875eaf068cfc1d2dbfcf20369b
Your name hereAndrius <iciukas@gmail.com>
2020-06-25 17:57:16 +0200

    raect docs

--- server.js
+++ server.js
@@ -23,10 +23,13 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
-app.use(express.static('client/build'));
 
-app.get('*', (req, res) => res.sendFile(path.resolve('client', 'build', 'index.html')));
+app.use(express.static(path.join(__dirname, 'build')));
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
 app.listen(port, () => {console.log(`server started on port ${port}`)});
 
 //DB config
