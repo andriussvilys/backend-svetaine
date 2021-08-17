bb5e7409d74713a919f1926dc24ab1253f2ca6da
Andrius Svilys <iciukas@gmail.com>
2021-08-17 22:25:52 +0300

    mongoDB fix

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://admin:Cimisraine15@cluster0-utbxr.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    // mongoURI: "mongodb+srv://admin:Cimisraine15@cluster0-utbxr.gcp.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "mongodb+srv://admin:Cimisraine15@cluster0.utbxr.gcp.mongodb.net/test?retryWrites=true&w=majority"
 }
