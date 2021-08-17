cb41e8b62f3274ba95f868be2c2ceee982725e50
Andrius Svilys <iciukas@gmail.com>
2021-08-18 00:56:40 +0300

    added build

---
+++ client/build/precache-manifest.4851a580a1b69cb1c8bd661bc300bb53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000110f7ea2176e5dfc752b160a3399e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1759c80cd9295c25d8d",
+    "url": "./static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "4834152dfad3333a2733",
+    "url": "./static/css/main.e717ade2.chunk.css"
+  },
+  {
+    "revision": "e1759c80cd9295c25d8d",
+    "url": "./static/js/2.e5d717fc.chunk.js"
+  },
+  {
+    "revision": "8a0f87d4ff9462a28d37926fe68f0ebe",
+    "url": "./static/js/2.e5d717fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4834152dfad3333a2733",
+    "url": "./static/js/main.c0d4de5d.chunk.js"
+  },
+  {
+    "revision": "11808710d3b9cbd7e7eb",
+    "url": "./static/js/runtime-main.2cfe3499.js"
+  }
+]);
